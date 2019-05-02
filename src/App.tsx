5169518641de6fac8be76dd2dc4adac0783f4a89
wesselexe <Ledigmand1>
2019-05-02 19:49:17 +0200

    Implement fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ExpenseView from './Components/ExpenseView/ExpenseView'
 
 function App() {
   return (
     <div className="App">
       <h1>Clean up</h1>
+      <ExpenseView />
     </div>
   );
 }
