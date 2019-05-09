b12b1f0a415c62863c23bcf3da06c4772287b70e
wesselexe <Ledigmand1>
2019-05-09 18:20:37 +0200

    cleaed up api file

--- src/Api/Api.tsx
+++ src/Api/Api.tsx
@@ -1,56 +1,61 @@
-const endpoint:string = "http://localhost:3000/expenses";
+const endpoint: string = "http://localhost:3000/expenses";
 
 // function to GET expenses from the API
-const fetchExpenses = ():any => {
+const fetchExpenses = (): any => {
     return fetch(endpoint)
-    .then(response => response.json())
-    .then((result) => {
-        if (result) {
-            return result
-        }
-    })
-}
-
-// function to GET all expenses from the API
-const fetchAll = async (numPages:number) => {
-    let midleResult:any[] = [];
-    for (let i = 0; i <= numPages; i++) {
-        await fetch(endpoint + "?limit=25&offset=" + (25 * i))
         .then(response => response.json())
         .then((result) => {
             if (result) {
-                result.expenses.forEach((it:any):void => {
-                    midleResult.push(it)
-                })
+                return result
             }
-        })    
+        })
+}
+
+// function to GET all expenses from the API
+const fetchAll = async (numPages: number) => {
+    let midleResult: any[] = [];
+    for (let i = 0; i <= numPages; i++) {
+        await fetch(endpoint + "?limit=25&offset=" + (25 * i))
+            .then(response => response.json())
+            .then((result) => {
+                if (result) {
+                    result.expenses.forEach((it: any): void => {
+                        midleResult.push(it)
+                    })
+                }
+            })
     }
     return midleResult
 }
 
 // Function to add a comment
-const addComment = (id:string, comment:string) => {
+const addComment = (id: string, comment: string) => {
     return fetch((endpoint + "/" + id), {
-        method: 'POST',
-        body: JSON.stringify({
-            comment: comment
-        }),
-        headers:{
-            'Content-Type': 'application/json'
-          }
-    }).then(res => res.json())
-    .catch(error => console.error('Error:', error));
+            method: 'POST',
+            body: JSON.stringify({
+                comment: comment
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error));
 }
 
 // Function to upload receipt
-const addReceipt = async (id:string, file:any) => {
+const addReceipt = async (id: string, file: any) => {
     let data = new FormData()
     data.append('receipt', file)
     return fetch((endpoint + "/" + id + "/receipts"), {
-        method: 'POST',
-        body: data
-    }).then(res => res.json())
-    .catch(error => console.error('Error:', error));
+            method: 'POST',
+            body: data
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error));
 }
 
-export { fetchExpenses, addComment, addReceipt, fetchAll };
+export {
+    fetchExpenses,
+    addComment,
+    addReceipt,
+    fetchAll
+};
