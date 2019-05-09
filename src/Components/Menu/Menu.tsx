12d9ac8b87f24518f5b11e57d20414492982d085
wesselexe <Ledigmand1>
2019-05-09 17:13:35 +0200

    added clear button to search

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FormControl } from 'react-bootstrap';
+import { FormControl, Button } from 'react-bootstrap';
 
 import './Menu.css'
 
@@ -15,15 +15,20 @@ class Menu extends React.Component<MenuProps> {
 
     handleChange = (event:any) => {
         this.setState({searchText: event.target.value})
-        console.log("test log")
         this.props.filter(event.target.value)
+    }
+
+    clearFilter = (event:React.MouseEvent) => {
+        this.setState({searchText: ""})
+        this.props.filter("")
     }
 
 
     render() {
         return (
             <div className="menu-box">
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={this.handleChange} />
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" value={this.state.searchText} onChange={this.handleChange} />
+                <Button variant="warning" onClick={this.clearFilter}>Clear</Button>
             </div>
         )
     }
