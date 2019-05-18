ab7dc3370682957d7aa4ee57d7ee6b6e3b919225
Diego <galmac12@gmail.com>
2019-05-18 16:52:58 +0200

    a bit of clean up

--- src/Api/Api.tsx
+++ src/Api/Api.tsx
@@ -1,6 +1,7 @@
-import { Expense, Response } from './Types'
+import { Expense, Response } from "./Types";
 
-const endpoint: string = "http://localhost:3000/expenses";
+const BASE_URL = "http://localhost:3000";
+const endpoint: string = `${BASE_URL}/expenses`;
 
 // function to GET expenses from the API
 
@@ -9,68 +10,71 @@ It would be nice here to add the correct types, this is where the data comes int
 You can use json2ts.com to convert the API response to a TS interface.
 */
 
-
-
 // Now `fetch` returns a promise of `Response`
-export const fetchExpenses = (numPage:number): Promise<Response> => {
-    return fetch(endpoint+ "?limit=25&offset=" + (25 * numPage))
-        .then(response => response.json())
-        .then((result) => {
-            if (result) {
-                result.expenses.forEach((it:Expense) => {
-                    if (it.receipts.length > 0) {  
-                        it.receipts = it.receipts.map(iter => {
-                            return "http://localhost:3000" + iter.url
-                        })
-                    }
-                })
-                return result
-            }
-        })
-}
+export const fetchExpenses = async (
+  numPage: number
+): Promise<Response | null> => {
+  const response = await fetch(`${endpoint}?limit=25&offset=${25 * numPage}`);
+  const result = await response.json();
+  if (result) {
+    result.expenses.forEach((it: Expense) => {
+      if (it.receipts.length > 0) {
+        it.receipts = it.receipts.map(iter => {
+          return `${BASE_URL}/${iter.url}`;
+        });
+      }
+    });
+    return result;
+  }
+  return null;
+};
 
 // function to GET all expenses from the API
 export const fetchAll = async (numPages: number) => {
-    let midleResult: any[] = [];
-    // Don't fetch too much data for nothing, fetch enough data to fill up the screen, then
-    // watch the scroll position and make an "infinite scroll" or add a simple pagination
-    for (let i = 0; i <= numPages; i++) {
-        await fetch(endpoint + "?limit=10&offset=" + (25 * i))
-            .then(response => response.json())
-            .then((result) => {
-                if (result) {
-                    result.expenses.forEach((it: any): void => {
-                        midleResult.push(it)
-                    })
-                }
-            })
+  const midleResult: any[] = [];
+  // Don't fetch too much data for nothing, fetch enough data to fill up the screen, then
+  // watch the scroll position and make an "infinite scroll" or add a simple pagination
+  for (let i = 0; i <= numPages; i++) {
+    const response = await fetch(`${endpoint}?limit=10&offset=${25 * i}`).then(
+      res => res.json()
+    );
+
+    if (response) {
+      response.expenses.forEach(
+        (it: any): void => {
+          midleResult.push(it);
+        }
+      );
     }
-    return midleResult
-}
+  }
+  return midleResult;
+};
 
 // Function to add a comment
 // Same here, add the return type
 export const addComment = (id: string, comment: string): Promise<Expense> => {
-    return fetch((endpoint + "/" + id), {
-            method: 'POST',
-            body: JSON.stringify({
-                comment: comment
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => res.json())
-        .catch(error => console.error('Error:', error));
-}
+  return fetch(`${endpoint}/${id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      comment: comment
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .catch(error => console.error("Error:", error));
+};
 
 // Function to upload receipt
 // Again return type
 export const addReceipt = async (id: string, file: any): Promise<Expense> => {
-    let data = new FormData()
-    data.append('receipt', file)
-    return fetch((endpoint + "/" + id + "/receipts"), {
-            method: 'POST',
-            body: data
-        }).then(res => res.json())
-        .catch(error => console.error('Error:', error));
-}
+  let data = new FormData();
+  data.append("receipt", file);
+  return fetch(`${endpoint}/${id}/receipts"`, {
+    method: "POST",
+    body: data
+  })
+    .then(res => res.json())
+    .catch(error => console.error("Error:", error));
+};
--- src/Components/ExpenseView/ExpenseView.tsx
+++ src/Components/ExpenseView/ExpenseView.tsx
@@ -1,138 +1,142 @@
 import React from "react";
-import { ExpenseRow } from '../ExpenseRow/ExpenseRow'
-import Menu from '../Menu/Menu'
-import './ExpenseView.css'
-import { fetchExpenses } from '../../Api/Api'
+import { ExpenseRow } from "../ExpenseRow/ExpenseRow";
+import Menu from "../Menu/Menu";
+import "./ExpenseView.css";
+import { fetchExpenses } from "../../Api/Api";
 
+interface State {
+  totalExpenses: any[];
+  shownExpenses: any[];
+  totalPages: number;
+  pagesLoaded: number;
+  pagesLoading: boolean;
+  isLoaded: boolean;
+  filter: string;
+}
 
-  interface State {
-    totalExpenses: any[],
-    shownExpenses: any[],
-    totalPages: number,
-    pagesLoaded: number,
-    pagesLoading: boolean,
-    isLoaded: boolean,
-    filter: string,
-} 
+const ValidateMe = (txt: string, filterText: string) => {
+  return txt.toLowerCase().indexOf(filterText.toLowerCase()) >= 0;
+};
 
-class ExpenseView extends React.Component {
-    state: State = {
-        totalExpenses: [],
-        shownExpenses: [],
-        totalPages: 0,
-        pagesLoaded: 0,
-        pagesLoading: false,
-        isLoaded: false,
-        filter: ""
-    };
+class ExpenseView extends React.Component<State> {
+  state: State = {
+    totalExpenses: [],
+    shownExpenses: [],
+    totalPages: 0,
+    pagesLoaded: 0,
+    pagesLoading: false,
+    isLoaded: false,
+    filter: ""
+  };
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.onScroll, false);
+  async componentDidMount() {
+    window.addEventListener("scroll", this.onScroll, false);
 
-        const request = async () => {
-            // Now with the correct types, response is of type `Response`
-            const response = await fetchExpenses(0);
-            this.setState({totalPages: Math.floor(response.total / 25)}) ;
-            await this.setState({
-                totalExpenses: response.expenses,
-                shownExpenses: response.expenses
-            })
+    // Now with the correct types, response is of type `Response`
+    const response = await fetchExpenses(0);
+    if (response) {
+      this.setState({ totalPages: Math.floor(response.total / 25) });
+      await this.setState({
+        totalExpenses: response.expenses,
+        shownExpenses: response.expenses
+      });
+    }
+    await this.setState({ isLoaded: true });
+  }
 
-            await this.setState({isLoaded: true})
-        }
-        request();
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
+
+  refresh = (id: string, receipt: boolean, comment: string): void => {
+    let indexOfExpense = this.state.totalExpenses.findIndex(it => it.id === id);
+    let NewTotalExpenses = this.state.totalExpenses;
+    if (receipt) {
+      NewTotalExpenses[indexOfExpense].receipts.push(
+        `http://localhost:3000/receipts/${id}-${
+          NewTotalExpenses[indexOfExpense].receipts.length
+        }`
+      );
+      this.setState({
+        totalExpenses: NewTotalExpenses
+      });
+    } else if (comment) {
+      NewTotalExpenses[indexOfExpense].comment = comment;
+      this.setState({
+        totalExpenses: NewTotalExpenses
+      });
+    }
+  };
+
+  loadMore = async () => {
+    this.setState({ pagesLoading: true });
+    const response = await fetchExpenses(this.state.pagesLoaded + 1);
+    if (response) {
+      await this.setState({
+        totalExpenses: this.state.totalExpenses.concat(response.expenses),
+        pagesLoaded: this.state.pagesLoaded + 1
+      });
     }
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.onScroll, false);
+    this.filterUsers(this.state.filter);
+  };
+
+  filterUsers = (filterText: string) => {
+    const filteredView = this.state.totalExpenses.filter(it => {
+      if (
+        ValidateMe(it.user.first, filterText) ||
+        ValidateMe(it.user.last, filterText) ||
+        ValidateMe(it.merchant, filterText) ||
+        ValidateMe(it.comment, filterText) ||
+        ValidateMe(it.amount, filterText)
+      ) {
+        return true;
       }
+      return false;
+    });
 
-    refresh = (id:string, receipt:boolean, comment:string):void => {
-        let indexOfExpense = this.state.totalExpenses.findIndex(it => it.id === id);
-        let NewTotalExpenses = this.state.totalExpenses;
-        if (receipt) {
-            NewTotalExpenses[indexOfExpense].receipts.push("http://localhost:3000/receipts/" + id + "-" + NewTotalExpenses[indexOfExpense].receipts.length)
-            this.setState({
-                totalExpenses: NewTotalExpenses
-            })
-        } else if (comment) {
-            NewTotalExpenses[indexOfExpense].comment = comment
-            this.setState({
-                totalExpenses: NewTotalExpenses
-            })
-        }
+    this.setState({
+      filter: filterText,
+      shownExpenses: filteredView,
+      pagesLoading: false
+    });
+  };
+
+  onScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 &&
+      this.state.pagesLoaded < this.state.totalPages &&
+      !this.state.pagesLoading
+    ) {
+      this.loadMore();
     }
+  };
 
-    loadMore = () => {
-        this.setState({pagesLoading: true})
-        const request = async (page:number) => {
-            const response = await fetchExpenses(page);
-            await this.setState({
-                totalExpenses: this.state.totalExpenses.concat(response.expenses),
-                pagesLoaded: this.state.pagesLoaded + 1
-            })
+  render() {
+    const { isLoaded, shownExpenses } = this.state;
 
-            this.filterUsers(this.state.filter); 
-        }
-
-        request(this.state.pagesLoaded + 1);
-    }
-
-    filterUsers = (filterText:string) => {
-        const filteredView = this.state.totalExpenses.filter((it) => {
-            if (it.user.first.toLowerCase().indexOf(filterText.toLowerCase()) >= 0) {
-                return true
-            } else if (it.user.last.toLowerCase().indexOf(filterText.toLowerCase()) >= 0) {
-                return true
-            } else if (it.merchant.toLowerCase().indexOf(filterText.toLowerCase()) >= 0) {
-                return true
-            } else if (it.comment.toLowerCase().indexOf(filterText.toLowerCase()) >= 0) {
-                return true
-            } else if (it.amount.value.toLowerCase().indexOf(filterText.toLowerCase()) >= 0) {
-                return true
-            }
-            return false
-        })
-
-        this.setState({
-            filter: filterText,
-            shownExpenses: filteredView,
-            pagesLoading: false
-        })
-    }
-
-    onScroll = () => {
-        if (
-          (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) &&
-          this.state.pagesLoaded < this.state.totalPages && !this.state.pagesLoading) {
-          this.loadMore();
-        }
-      }
-
-    render() {
-        const { isLoaded, shownExpenses } = this.state;
-
-        if (isLoaded) {
+    if (isLoaded) {
+      return (
+        <div>
+          <Menu
+            filterFunction={this.filterUsers}
+            filterText={this.state.filter}
+          />
+          {shownExpenses.map(data => {
             return (
-                <div>
-                    <Menu filterFunction={this.filterUsers} filterText={this.state.filter} />
-                    {
-                        shownExpenses.map((data) => {
-                            return <ExpenseRow key={data.index} refresh={this.refresh} id={data.id} amount={data.amount} date={data.date} merchant={data.merchant} receipts={data.receipts} comment={data.comment} category={data.category} user={data.user} index={data.index}/>
-                        })
-
-                        
-                    }
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <h1>LOADING...</h1>
-                </div>
-            )
-        }
+              <ExpenseRow key={data.index} refresh={this.refresh} {...data} />
+            );
+          })}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>LOADING...</h1>
+        </div>
+      );
     }
+  }
 }
 
 export default ExpenseView;
